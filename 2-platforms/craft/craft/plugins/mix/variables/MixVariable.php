e6efac3ced2fe768a6fed08c4f8b595f346e8296
Joris Noordermeer <jo.noordermeer@gmail.com>
2017-05-25 17:13:19 +0200

    Fix the Mix Plugin when used with versioned assets

--- 2-platforms/craft/craft/plugins/mix/variables/MixVariable.php
+++ 2-platforms/craft/craft/plugins/mix/variables/MixVariable.php
@@ -4,29 +4,34 @@ namespace Craft;
 class MixVariable
 {
 
-	function bust($file)
-	{
+    function bust($file)
+    {
         $time = (file_exists($_SERVER['DOCUMENT_ROOT'] . '/' . $file)) ? filemtime($_SERVER['DOCUMENT_ROOT'] . '/' . $file) : '';
-		return '/' . $file . '?' . $time;
-	}
+        return '/' . $file . '?' . $time;
+    }
 
     public function mix($file)
     {
+        // TODO: No checks concerning the file path are made.
+        // It's just assumed that we are pointing to the public directory
+
+        // Prepend public folder to path
         $path = "/public/{$file}";
 
-        if (! file_exists($manifestPath = $_SERVER['DOCUMENT_ROOT'] . '/../mix-manifest.json')) {
-            return $this->bust($file);
-        }
+        // Check if the manifest exists
+        if (! file_exists($manifestPath = $_SERVER['DOCUMENT_ROOT'] . '/../mix-manifest.json')) return $this->bust($file);
 
+        // Read the manifest
         $manifest = json_decode(file_get_contents($manifestPath), true);
 
-        if (! array_key_exists($path, $manifest)) {
-            return $this->bust($file);
-        }
+        // Check if the requested file is listed in the manifest
+        if (! array_key_exists($path, $manifest)) return $this->bust($file);
 
+        // Check if the file was actually versioned
         if($manifest[$path] == $path) return $this->bust($file);
 
-        return $manifest[$path];
+        // Remove 'public' from the output
+        return str_replace('/public/', '/', $manifest[$path]);
     }
 
 }
