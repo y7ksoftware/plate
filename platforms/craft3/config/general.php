92a1e755985d3803de438aa7d4ac2c02cb0f20fd
Joris Noordermeer <jo.noordermeer@gmail.com>
2018-06-19 16:34:55 +0200

    Craft 3 config file formatting

--- platforms/craft3/config/general.php
+++ platforms/craft3/config/general.php
@@ -10,45 +10,43 @@
 
 return [
 
-    '*' => [
-        'devMode' => getenv('APP_DEBUG') === 'true',
-        'enableTemplateCaching' => getenv('TEMPLATE_CACHING') === 'true',
-        'securityKey' => getenv('SECURITY_KEY'),
-        'omitScriptNameInUrls' => true,
-        'convertFilenamesToAscii' => true, // Convert upload filenames to valid ascii chars
-        'limitAutoSlugsToAscii' => true, // Converts Umlauts etc. in slugs to ascii
-        'enableCsrfProtection' => true, // Enable CSRF protection (will be default in Craft 3)
-        'cacheDuration' => 'P2W', // Set default template caching time to 2 weeks
-        'generateTransformsBeforePageLoad' => true, // Generate Image Transforms before page load. Else caching may be a problem
-        'sendPoweredByHeader' => false, // Disable sending a "Powered by Craft" header
-        'defaultWeekStartDay' => 1, // Start Week on Monday
-        'allowUpdates' => (getenv('APP_ENV') === 'local') ? 'minor-only' : false, // Disable auto-updates on servers
-        'timezone' => 'Europe/Zurich', // Set Timezone to Zurich
-        'cpTrigger' => 'admin',
-        'userSessionDuration' => (getenv('APP_ENV') === 'local') ? false : 'PT4H',
+    'devMode' => getenv('APP_DEBUG') === 'true',
+    'enableTemplateCaching' => getenv('TEMPLATE_CACHING') === 'true',
+    'securityKey' => getenv('SECURITY_KEY'),
+    'omitScriptNameInUrls' => true,
+    'convertFilenamesToAscii' => true, // Convert upload filenames to valid ascii chars
+    'limitAutoSlugsToAscii' => true, // Converts Umlauts etc. in slugs to ascii
+    'enableCsrfProtection' => true, // Enable CSRF protection (will be default in Craft 3)
+    'cacheDuration' => 'P2W', // Set default template caching time to 2 weeks
+    'generateTransformsBeforePageLoad' => true, // Generate Image Transforms before page load. Else caching may be a problem
+    'sendPoweredByHeader' => false, // Disable sending a "Powered by Craft" header
+    'defaultWeekStartDay' => 1, // Start Week on Monday
+    'allowUpdates' => (getenv('APP_ENV') === 'local') ? 'minor-only' : false, // Disable auto-updates on servers
+    'timezone' => 'Europe/Zurich', // Set Timezone to Zurich
+    'cpTrigger' => 'admin',
+    'userSessionDuration' => (getenv('APP_ENV') === 'local') ? false : 'PT4H',
 
-        // Restrict upload to certain extensions
-        // 'allowedFileExtensions' => 'json, jpg, jpeg, png, gif, pdf, zip',
-        // Allow upload of Json files, also used for external Video Plugin
-        'extraAllowedFileExtensions' => 'json',
+    // Restrict upload to certain extensions
+    // 'allowedFileExtensions' => 'json, jpg, jpeg, png, gif, pdf, zip',
+    // Allow upload of Json files, also used for external Video Plugin
+    'extraAllowedFileExtensions' => 'json',
 
-        'siteUrl' => [
-            'default' => getenv('APP_URL') . '/',
-            // 'de' => getenv('APP_URL') . '/de/',
-        ],
-
-        /*
-         * Custom Settings
-         */
-        'appEnv' => getenv('APP_ENV'),
-        'bugsnagFrontendApiKey' => getenv('BUGSNAG_API_KEY_JS'),
-
-        'analytics' => [
-            'matomoSiteId' => getenv('MATOMO_SITE_ID'),
-            'googleAnalyticsCode' => getenv('GOOGLE_ANALYTICS_CODE'),
-            'googleVerificationCode' => getenv('GOOGLE_VERIFICATION_CODE'),
-        ],
-
+    'siteUrl' => [
+        'default' => getenv('APP_URL') . '/',
+        // 'de' => getenv('APP_URL') . '/de/',
     ],
+
+    /*
+     * Custom Settings
+     */
+    'appEnv' => getenv('APP_ENV'),
+    'bugsnagFrontendApiKey' => getenv('BUGSNAG_API_KEY_JS'),
+
+    'analytics' => [
+        'matomoSiteId' => getenv('MATOMO_SITE_ID'),
+        'googleAnalyticsCode' => getenv('GOOGLE_ANALYTICS_CODE'),
+        'googleVerificationCode' => getenv('GOOGLE_VERIFICATION_CODE'),
+    ],
+
 
 ];
