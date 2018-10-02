344d96c3d0d85667d5390acbcef31c58d007a1f5
Robert Krieg <hello@robertkrieg.ch>
2018-10-02 14:21:44 +0200

    add missing config to backend->frontend constants

--- platforms/laravel/resources/views/helpers/frontend-constants.blade.php
+++ platforms/laravel/resources/views/helpers/frontend-constants.blade.php
@@ -1,7 +1,7 @@
 {{--  Globals --}}
 <script>
     window.backendConfig = {
-        'CSRF_TOKEN': '{!! $backendConfig['CSRF_TOKEN'] !!}',
+        'CSRF_TOKEN': '{!! csrf_token() !!}',
         'APP_ENV': '{!! $backendConfig['APP_ENV'] !!}',
         'APP_DEBUG': '{!! $backendConfig['APP_DEBUG'] !!}',
         'BUGSNAG_API_KEY': '{!! $backendConfig['BUGSNAG_API_KEY'] !!}',
