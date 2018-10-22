0cc18a206c5d62ad68108d546c9f0b689f60eaf2
Robert Krieg <hello@robertkrieg.ch>
2018-10-22 16:24:12 +0200

    add imagetransforms config

---
+++ platforms/craft3/config/imagetransforms.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'default' => [
+        ['width' => 1920, 'jpegQuality' => 80],
+        ['width' => 675, 'jpegQuality' => 80],
+        ['width' => 100, 'jpegQuality' => 40],
+    ],
+];
