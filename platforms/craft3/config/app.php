5f825e6ed9c3a95e2fedba79b2e1d4d4a2801ac7
Robert Krieg <hello@robertkrieg.ch>
2018-11-13 11:00:47 +0100

    Add craft email config to config files

--- platforms/craft3/config/app.php
+++ platforms/craft3/config/app.php
@@ -15,4 +15,26 @@ return [
         ],
     ],
     'bootstrap' => ['environment-indicator-module', 'maintenance-module'],
+
+    'components' => [
+        'mailer' => function() {
+            // Get the stored email settings
+            $settings = Craft::$app->systemSettings->getEmailSettings();
+
+            // Override the transport adapter class
+            $settings->transportType = craft\mail\transportadapters\Smtp::class;
+
+            // Override the transport adapter settings
+            $settings->transportSettings = [
+                'host' => getenv('EMAIL_HOST'),
+                'port' => getenv('EMAIL_PORT'),
+                'username' => getenv('EMAIL_USER'),
+                'password' => getenv('EMAIL_PASSWORD'),
+                'useAuthentication' => true,
+                'encryptionMethod' => 'tls',
+            ];
+
+            return craft\helpers\MailerHelper::createMailer($settings);
+        },
+    ]
 ];
